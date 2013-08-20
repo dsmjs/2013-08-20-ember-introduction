70ca15a54d358764ee909c9a0a5a7be506de7dbd
toran billups <toranb@gmail.com>
2013-08-19 19:38:37 -0500

    step 1) add everything (even when optional)

--- js/app.js
+++ js/app.js
@@ -1,1 +1,31 @@
 App = Ember.Application.create();
+
+App.Router.map(function() {
+    this.resource("people", { path: "/" });
+});
+
+App.PeopleRoute = Ember.Route.extend({
+    model: function() {
+        return App.Person.find();
+    }
+});
+
+App.Person = Ember.Object.extend({
+    firstName: ''
+});
+
+App.Person.reopenClass({
+    find: function() {
+        var first = App.Person.create({firstName: 'toran'});
+        var last = App.Person.create({firstName: 'matt'});
+        return [first, last];
+    }
+});
+
+App.PeopleController = Ember.ArrayController.extend({
+    content: null
+});
+
+App.PeopleView = Ember.View.extend({
+    defaultTemplate: Ember.Handlebars.compile("<h1>hello</h1>")
+});
